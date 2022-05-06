d362c89eefd77450ec841cefe1c7867e6a9e3c78
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-06 17:13:52 +0700

    set actiityresultlauncher

--- app/src/main/java/com/example/intentimplicitcamera06052022/MainActivity.java
+++ app/src/main/java/com/example/intentimplicitcamera06052022/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.intentimplicitcamera06052022;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    private ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                }
+            }
+    );
 }
