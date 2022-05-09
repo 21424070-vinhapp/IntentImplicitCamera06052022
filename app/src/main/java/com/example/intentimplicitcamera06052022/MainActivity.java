ddcf303ca5df9f566328d6a303d447dc77350637
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-09 13:31:04 +0700

    kiem tra xem nguoi dung chap nhan truy cap camera hay chua

--- app/src/main/java/com/example/intentimplicitcamera06052022/MainActivity.java
+++ app/src/main/java/com/example/intentimplicitcamera06052022/MainActivity.java
@@ -5,27 +5,54 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mBtnCamera, mBtnGallery;
+    ImageView mImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        init();
+
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 //checkSelfPermission KEIM TRA XEM nguoi dung co chap nhan quyen truy cap vao camera hay chua
+                if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CAMERA)!=
+                PackageManager.PERMISSION_GRANTED){
+
+                }
+            }
+        });
+
     }
 
-    private ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+    private void init()
+    {
+        mBtnCamera=findViewById(R.id.buttonCamera);
+        mBtnGallery=findViewById(R.id.buttonGallery);
+        mImg=findViewById(R.id.imageView);
+    }
+
+    private ActivityResultLauncher<Intent> cameraLauncher=registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
 
                 }
-            }
-    );
+            });
 }
