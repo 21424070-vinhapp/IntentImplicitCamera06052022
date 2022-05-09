c1c47415e6f2d8b8e0b78eaa21177b63351a3504
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-09 14:32:15 +0700

    kiem tra va thong bao cho nguoi dung chap nhan quyen truy cap camera

--- app/src/main/java/com/example/intentimplicitcamera06052022/MainActivity.java
+++ app/src/main/java/com/example/intentimplicitcamera06052022/MainActivity.java
@@ -4,55 +4,92 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button mBtnCamera, mBtnGallery;
     ImageView mImg;
+    int REQUEST_CODE_CAMERA=123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        init();
+
+        intit();
 
         mBtnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 //checkSelfPermission KEIM TRA XEM nguoi dung co chap nhan quyen truy cap vao camera hay chua
-                if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CAMERA)!=
-                PackageManager.PERMISSION_GRANTED){
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)!=
+                PackageManager.PERMISSION_GRANTED)
+                {
+                    //Kiem tra xem nguoi dung co bam deny chua
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,Manifest.permission.CAMERA))
+                    {
+                        Log.d("BBB", "vao day");
+                    }
 
+                    //neu khong thi hien ra bang thong bao yeu cau nguoi dung cung cap quyen truy cap camera
+                    else
+                    {
+                        ActivityCompat.requestPermissions(
+                                MainActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                REQUEST_CODE_CAMERA);
+                    }
                 }
             }
         });
 
+
     }
 
-    private void init()
-    {
+    private void intit() {
         mBtnCamera=findViewById(R.id.buttonCamera);
         mBtnGallery=findViewById(R.id.buttonGallery);
         mImg=findViewById(R.id.imageView);
     }
 
-    private ActivityResultLauncher<Intent> cameraLauncher=registerForActivityResult(
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==REQUEST_CODE_CAMERA)
+        {
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "cho PHEP", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this, "Khong cho phep", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private ActivityResultLauncher<Intent> cameraLauncher= registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
 
                 }
-            });
+            }
+    );
 }
